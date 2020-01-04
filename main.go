1bdf994f0a66c15b8a5a18b8606d26bab7a21afe
Muhammadnumon <mnumojon@mail.ru>
2020-01-04 13:02:59 +0300

    hhvddf

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println( "Hello go")
+	score1:=10
+	score2:=10
+	score3:=10
+promotersLowerBound :=9
+if score1 >=promotersLowerBound{
+
+
+}
+
+}
+
+
+
+
+//ctrl+alt+v
+//alt +f12->Terminal
+//ctrl+/ ->comment avto
+//tochka ostanovki->Ctrl+f8
